f285c719cbcf461ebfbfd73c190daa2e932a678e
Alexander Bulaev <aleks.bulaev@gmail.com>
2013-10-11 18:26:13 +0400

    Safely parsing Location

--- src/main/scala/me/alexbool/streamstorm/instagram/parsers/LocationSafeParser.scala
+++ src/main/scala/me/alexbool/streamstorm/instagram/parsers/LocationSafeParser.scala
@@ -3,10 +3,10 @@ package me.alexbool.streamstorm.instagram.parsers
 import spray.json._
 import me.alexbool.streamstorm.instagram.Location
 
-class LocationParser extends RootJsonReader[Location] {
+class LocationSafeParser extends RootJsonReader[Option[Location]] {
   def read(json: JsValue) = json.asJsObject.getFields("latitude", "longitude") match {
-    case Seq(JsNumber(lat), JsNumber(long)) =>
-      Location(BigDecimal(lat.toDouble), BigDecimal(long.toDouble))
+    case Seq(JsNumber(lat), JsNumber(long)) => Some(Location(BigDecimal(lat.toDouble), BigDecimal(long.toDouble)))
+    case Seq()                              => None
     case _ => deserializationError(s"Cannot parse location: ${json.compactPrint}")
   }
 }
--- src/main/scala/me/alexbool/streamstorm/instagram/parsers/MediaParser.scala
+++ src/main/scala/me/alexbool/streamstorm/instagram/parsers/MediaParser.scala
@@ -7,7 +7,7 @@ import me.alexbool.streamstorm.instagram.Media
 class MediaParser extends RootJsonReader[Media] {
   def read(json: JsValue) = json.asJsObject.getFields("id", "tags", "location", "created_time") match {
     case Seq(JsString(id), JsArray(tags), JsObject(location), JsString(createdMillis)) =>
-      Media(id, tags.map(_.asInstanceOf[JsString].value), Some((new LocationParser).read(JsObject(location))), new Instant(createdMillis.toLong))
+      Media(id, tags.map(_.asInstanceOf[JsString].value), (new LocationSafeParser).read(JsObject(location)), new Instant(createdMillis.toLong))
     case Seq(JsString(id), JsArray(tags), JsNull, JsString(createdMillis)) =>
       Media(id, tags.map(_.asInstanceOf[JsString].value), None, new Instant(createdMillis.toLong))
     case _ => deserializationError("Cannot deserialize media")

90001ff51fe4bfcdcc8c3655ccace63f0ccdb89e
Alexander Bulaev <aleks.bulaev@gmail.com>
2013-10-12 19:04:15 +0400

    Save images to mongo

--- src/main/scala/me/alexbool/streamstorm/instagram/MediaDao.scala
+++ src/main/scala/me/alexbool/streamstorm/instagram/MediaDao.scala
@@ -10,9 +10,17 @@ class MediaDao(collection: BSONCollection)(implicit ec: ExecutionContext) {
     val doc = BSONDocument(
       "_id"         -> media.id,
       "tags"        -> media.tags,
+      "images"      -> BSONDocument(
+        "low"   -> mapImage(media.images.lowResolution),
+        "std"   -> mapImage(media.images.standardResolution),
+        "thumb" -> mapImage(media.images.thumbnail)
+      ),
       "createdTime" -> BSONDateTime(media.createdTime.getMillis),
       "location"    -> media.location.map(loc => BSONArray(loc.longitude.toString(), loc.latitude.toString()))
     )
     collection.save(doc)
   }
+
+  private def mapImage(img: Image) =
+    BSONDocument("width" -> img.width, "height" -> img.height, "url" -> img.url)
 }

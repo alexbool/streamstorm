92ff21448ced8add0299f2f520a638e87083f667
Alexander Bulaev <aleks.bulaev@gmail.com>
2013-10-12 20:14:33 +0400

    Make dao functional

--- src/main/scala/me/alexbool/streamstorm/instagram/MediaDao.scala
+++ src/main/scala/me/alexbool/streamstorm/instagram/MediaDao.scala
@@ -1,26 +1,61 @@
 package me.alexbool.streamstorm.instagram
 
+import scala.concurrent.{Future, ExecutionContext}
+import reactivemongo.bson._
 import reactivemongo.api.collections.default.BSONCollection
-import scala.concurrent.ExecutionContext
-import reactivemongo.bson.{BSONArray, BSONDateTime, BSONDocument}
+import org.joda.time.Instant
 
 class MediaDao(collection: BSONCollection)(implicit ec: ExecutionContext) {
 
-  def save(media: Media) {
-    val doc = BSONDocument(
+  implicit private val mapper = new MediaMapper
+
+  def save(media: Media) = collection.save(media)
+
+  def findById(id: String): Future[Option[Media]] = {
+    collection.find(BSONDocument("_id" -> id)).cursor[Media].headOption()
+  }
+
+  private class MediaMapper extends BSONDocumentReader[Media] with BSONDocumentWriter[Media] {
+    def write(media: Media) = BSONDocument(
       "_id"         -> media.id,
       "tags"        -> media.tags,
       "images"      -> BSONDocument(
-        "low"   -> mapImage(media.images.lowResolution),
-        "std"   -> mapImage(media.images.standardResolution),
-        "thumb" -> mapImage(media.images.thumbnail)
+        "low"   -> writeImage(media.images.lowResolution),
+        "std"   -> writeImage(media.images.standardResolution),
+        "thumb" -> writeImage(media.images.thumbnail)
       ),
       "createdTime" -> BSONDateTime(media.createdTime.getMillis),
       "location"    -> media.location.map(loc => BSONArray(loc.longitude.toString(), loc.latitude.toString()))
     )
-    collection.save(doc)
-  }
 
-  private def mapImage(img: Image) =
-    BSONDocument("width" -> img.width, "height" -> img.height, "url" -> img.url)
+    private def writeImage(img: Image) =
+      BSONDocument("width" -> img.width, "height" -> img.height, "url" -> img.url)
+
+    def read(bson: BSONDocument): Media = {
+      val imgsDoc = bson.getAs[BSONDocument]("images").get
+      val images = Images(
+        lowResolution = readImage(imgsDoc.getAs[BSONDocument]("low").get),
+        standardResolution =  readImage(imgsDoc.getAs[BSONDocument]("std").get),
+        thumbnail = readImage(imgsDoc.getAs[BSONDocument]("thumb").get)
+      )
+      Media(
+        id          = bson.get("_id").get.asInstanceOf[BSONString].value,
+        tags        = bson.get("tags").get.asInstanceOf[BSONArray].values.map(_.asInstanceOf[BSONString].value),
+        location    = bson.getAs[BSONArray]("location").map(readLocation),
+        images      = images,
+        createdTime = new Instant(bson.getAs[BSONDateTime]("createdTime").get.value)
+      )
+    }
+
+    private def readImage(doc: BSONDocument) = Image(
+      width  = doc.getAs[BSONInteger]("width").get.value,
+      height = doc.getAs[BSONInteger]("height").get.value,
+      url    = doc.getAs[BSONString]("url").get.value
+    )
+
+    private def readLocation(array: BSONArray) = Location(
+      longitude = BigDecimal(array.getAs[BSONString](0).get.value),
+      latitude  = BigDecimal(array.getAs[BSONString](1).get.value)
+    )
+  }
 }
---
+++ src/test/scala/me/alexbool/streamstorm/instagram/MediaDaoSpec.scala
@@ -0,0 +1,37 @@
+package me.alexbool.streamstorm.instagram
+
+import akka.actor.ActorSystem
+import com.typesafe.config.ConfigFactory
+import org.scalatest.{Matchers, WordSpec}
+import org.joda.time.Instant
+import reactivemongo.api.MongoDriver
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+class MediaDaoSpec extends WordSpec with Matchers {
+  val config = ConfigFactory.load("application.conf")
+  val system = ActorSystem("MediaDaoSpec")
+  implicit val ec = system.dispatcher
+  val driver = new MongoDriver(system)
+  val connection = driver.connection(Seq("localhost"))
+  val db = connection("streamstorm")
+  val collection = db("media")
+  val dao = new MediaDao(collection)(system.dispatcher)
+
+  "MediaDao" should {
+    "save Media instances" in {
+      val media = Media(
+        id          = "123456",
+        tags        = Seq("rain"),
+        location    = Some(Location(BigDecimal(90), BigDecimal(90))),
+        images      = Images(
+          lowResolution      = Image(360, 360, "http://google.com"),
+          standardResolution = Image(640, 640, "http://google.com"),
+          thumbnail          = Image(150, 150, "http://google.com")
+        ),
+        createdTime = new Instant
+      )
+      Await.result(dao.save(media), 15.seconds).ok should be (true)
+    }
+  }
+}
